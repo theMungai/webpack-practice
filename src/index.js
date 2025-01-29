69b9967bd1f0f2b4f284ab76139e1b37a4e303b1
theMungai <mungaithesimon@gmail.com>
2025-01-29 07:50:55 +0300

    Webpack loading images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import { greeting } from "./greeting.js"
+import "./styles.css";
+import { greeting } from "./greeting.js";
+import odinImage from "./odin.jpg";
+
+console.log(greeting)
+
+const image = document.createElement("img");
+image.src = odinImage
+
+document.body.appendChild(image)
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { watchFile } = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
@@ -9,9 +10,30 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true
     },
+
+    devtool: "eval-source-map",
+    devServer:{
+        watchFiles:["./src/template.html"],
+    },
     plugins : [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
     ],
+    module: {
+        rules : [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            }
+        ],
+    },
 };
